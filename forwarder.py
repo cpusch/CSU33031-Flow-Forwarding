57ea5b252f69b26b4ff2c4e9119bbe57d2c99953
cpusch <conpusch@yahoo.com>
2022-11-14 00:23:04 +0000

    fixed if statement not hitting

--- forwarder.py
+++ forwarder.py
@@ -5,7 +5,7 @@ from constants import *
 localIP = sys.argv[1]
 localPort   = 54321
 bufferSize  = 1024
-serverIP = ('server','54321')
+serverIP = ('server', 54321)
 
 UDPForwardSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPForwardSocket.bind((localIP, localPort))
@@ -17,12 +17,13 @@ while(True):
     clientAddress = bytesAddressPair[1]
 
     header = clientMessage[:4]
+    print(header)
     clientMessage = clientMessage[4:]
     print(f"packet received. Destination is node code: 2 [server]")
-    if localIP == FORWARDER_IPS['pdf']:
+    if localIP == FORWARDER_IPS['pdf'][0]:
         UDPForwardSocket.sendto(DESTINATION_HEADER+NODE_CODES['server']+clientMessage, FORWARDER_IPS['txt'])
         print(f"Packet received forwarding...")
-    elif localIP == FORWARDER_IPS['txt']:
+    elif localIP == FORWARDER_IPS['txt'][0]:
         print(f"Packet received last forwarder, sending to server")
         UDPForwardSocket.sendto(DESTINATION_HEADER+NODE_CODES['server']+clientMessage, serverIP)
 
