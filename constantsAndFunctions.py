b95c38c8d2c697a1526b3618e5b8fddbd563fa32
cpusch <conpusch@yahoo.com>
2022-12-07 19:20:51 +0000

    added function for later use

--- constantsAndFunctions.py
+++ constantsAndFunctions.py
@@ -5,8 +5,11 @@ ROUTER_IPS =    {'R1':('workerPDF',COM_PORT),
 
 ENDPOINT_IPS = {'E1':('client',COM_PORT),
                 'E2':('client2',COM_PORT),
-                'E3':('server',(COM_PORT))}
+                'E3':('server',COM_PORT)}
 
 HEADERS =   {'dest':b'DST',
              'length':b'LEN',
              'reqTable':b'REQ'}
+
+def FindForwarder():
+    return ""
