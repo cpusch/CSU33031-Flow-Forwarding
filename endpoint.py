5220d28ad5c2b7cf2add2d7b36598150e0d71fa1
cpusch <conpusch@yahoo.com>
2022-12-07 19:18:53 +0000

    all endpoints now share same source code

--- endpoint.py
+++ endpoint.py
@@ -1,13 +1,14 @@
 import socket
-from constants import *
+from constantsAndFunctions import *
+from sys import argv
 
-localip = ('client',54321)
-bytesToSend = b'hello endpoint'
+
+localIP = ENDPOINT_IPS[argv[1]][0]
 forwarderAddress = (ROUTER_IPS['R1'])
 bufferSize = 1024
 
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPClientSocket.bind(localip)
+UDPClientSocket.bind(localIP)
 
 UDPClientSocket.sendto(HEADERS['dest']+ENDPOINT_IPS['E3']+bytesToSend, forwarderAddress)
 print(f"Message: '{bytesToSend}' sent to server")
