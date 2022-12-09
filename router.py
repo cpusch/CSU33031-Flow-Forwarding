ca1b81258ce2b7d8ec3639aebf9aa2dbfc501280
cpusch <conpusch@yahoo.com>
2022-12-09 22:15:47 +0000

    created logic to handle different headers

--- router.py
+++ router.py
@@ -1,30 +1,42 @@
 import socket
 from sys import argv
-from constantsAndFunctions import *
+from constants import *
+from controller import NODECODE_TO_HOSTNAME
 
-localIP = argv[1]
-localPort   = COM_PORT
-bufferSize  = 1024
-destinationIP = None
 
-UDPForwardSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPForwardSocket.bind((localIP, localPort))
-print("Forwarder up and listening")
 
-while(True):
-    bytesAddressPair = UDPForwardSocket.recvfrom(bufferSize)
-    clientMessage = bytesAddressPair[0]
-    clientAddress = bytesAddressPair[1]
 
-    header = clientMessage[:4]
-    print(header)
-    clientMessage = clientMessage[4:]
-    if localIP == FORWARDER_IPS['pdf'][0]:
-        UDPForwardSocket.sendto(DESTINATION_HEADER+NODE_CODES['server']+clientMessage, FORWARDER_IPS['txt'])
-        print(f"Packet received forwarding...")
-    elif localIP == FORWARDER_IPS['txt'][0]:
-        print(f"Packet received last forwarder, sending to server")
-        UDPForwardSocket.sendto(DESTINATION_HEADER+NODE_CODES['server']+clientMessage, serverIP)
+def main():
+    # routing table with have format of 'nodeCode of dest':IP
+    ROUTING_TABLE = {}
+    localIP = NODECODE_TO_HOSTNAME[argv[1]][0]
+    localPort   = COM_PORT
+    bufferSize  = 1024
+    destinationIP = None
+
+    UDPForwardSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    UDPForwardSocket.bind((localIP, localPort))
+    print("Forwarder up and listening")
+
+    # always true and waiting for new packets to com in
+    while(True):
+        bytesAddressPair = UDPForwardSocket.recvfrom(bufferSize)
+        # encoded message
+        encMessage = bytesAddressPair[0]
+        header = encMessage[:4]
+
+        if header == HEADERS['message']:
+            destinationCode = encMessage[4:7]
+            payload = encMessage[7:]
+        elif header == HEADERS['tableUpdate']:
+            pass
+        elif header == HEADERS['noDestination']:
+            print("Destination not found dropping packet")
+
+
+
+if __name__ == "__main__":
+    main()
 
 
     
