11270428684242c14fe6c88854e41dd96c865aa8
cpusch <conpusch@yahoo.com>
2022-12-09 17:46:00 +0000

    added headers

--- constants.py
+++ constants.py
@@ -2,4 +2,5 @@ COM_PORT = 54321
 HEADERS =   {'dest':b'DST',
              'length':b'LEN',
              'reqTable':b'REQ',
-             'tableUpdate':b'TUP'}
+             'tableUpdate':b'TUP',
+             'noDestination':b'DNF'}
