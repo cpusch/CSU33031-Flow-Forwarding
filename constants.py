dad3c876a707f0b900f96643790f25bd30afcadb
cpusch <conpusch@yahoo.com>
2022-12-09 22:15:56 +0000

    removed unused headers

--- constants.py
+++ constants.py
@@ -1,6 +1,5 @@
 COM_PORT = 54321
-HEADERS =   {'dest':b'DST',
-             'length':b'LEN',
+HEADERS =   {'message':b'MSG',
              'reqTable':b'REQ',
              'tableUpdate':b'TUP',
              'noDestination':b'DNF'}
